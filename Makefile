d578c30d87b3cd7ca07926f44fdc01d2f66321cd
Ian Sinnott <ian@iansinnott.com>
2017-12-29 13:34:54 -0800

    Rename tmp dir to data

--- Makefile
+++ Makefile
@@ -4,16 +4,16 @@ target_dir = $(shell ls -t $(base_dir))
 
 target_path = $(base_dir)/$(target_dir)
 
-tmp:
-	mkdir -p ./tmp
+data:
+	mkdir -p ./data
 
-./tmp/$(target_dir):
-	@cp -R $(target_path) ./tmp
+./data/$(target_dir):
+	@cp -R $(target_path) ./data
 
-import_backup: tmp ./tmp/$(target_dir)
+import_backup: data ./data/$(target_dir)
 
 import_and_connect: import_backup
 	@./connect.sh __test_connection
 
 show_db_file:
-	@echo "Current database location:\n$(shell find $(PWD)/tmp -iname 3d0d7e5fb2ce288813306e4d4636395e047a3d28 | head -n 1)"
+	@echo "Current database location:\n$(shell find $(PWD)/data -iname 3d0d7e5fb2ce288813306e4d4636395e047a3d28 | head -n 1)"
--- connect.sh
+++ connect.sh
@@ -3,14 +3,14 @@ set -euo pipefail
 IFS=$'\n\t'
 # What's this gibberish?? http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
-if [[ ! -e ./tmp ]]; then
+if [[ ! -e ./data ]]; then
 	echo 'Temp directory does not yet exist. Run `make import_and_connect` then try again.'
 	exit 1
 fi
 
 # The head -n1 should not be necessary (right?). But in case there are multiple
 # files just take the first one
-DB_FILE=$(find ./tmp -iname 3d0d7e5fb2ce288813306e4d4636395e047a3d28 | head -n 1)
+DB_FILE=$(find ./data -iname 3d0d7e5fb2ce288813306e4d4636395e047a3d28 | head -n 1)
 
 if [[ ! -e "$DB_FILE" ]]; then
 	echo "Could not find database file. Ensure you have first run: make import_and_connect"
